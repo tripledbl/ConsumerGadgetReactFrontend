1466ef6c6941409566d9352b941917dc8f28ee50
Peaches255 <74527224+Peaches255@users.noreply.github.com>
2021-10-27 12:42:09 -0700

    cgt-3 created starter react project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Tripledbl is the best team ever!!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
