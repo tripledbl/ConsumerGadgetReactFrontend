441d1602e9b3f077168ecec30b5680efa48b9e40
Adrian Unruh <unruha@uw.edu>
2021-10-27 23:29:32 -0700

    changed app name and replaced homepage cards

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,8 +7,8 @@ function HeroSection() {
   return (
     <div className='hero-container'>
       <video src='/videos/video-1.mp4' autoPlay loop muted />
-      <h1>ConsumerGadget</h1>
-      <p>Predictive Modelling made Accessible</p>
+      <h1>Predictant</h1>
+      <p>Predictive Modeling Made Accessible</p>
       <div className='hero-btns'>
         <Button
           className='btns'
