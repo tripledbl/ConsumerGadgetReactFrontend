df8b5c380d226277cc2f5ad24e320c7f28519f7c
Adrian Unruh <unruha@uw.edu>
2021-10-27 18:44:47 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-"# ConsumerGadgetReactFrontend" 
+# ConsumerGadgetReactFrontend
+## Install Dependencies (prequistic: npm installed)
+npm install
+npm run build
+## Run application at http://localhost:3000/
+npm start
---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import './App.css';
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Models from './components/pages/Models';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/models' component={Models} />
+          <Route path='/products' component={Products} />
+          <Route path='/sign-up' component={SignUp} />
+        </Switch>
+      </Router>
+    </>
+  );
+}
+
+export default App;
