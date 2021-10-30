582b47e06b59c3761886d2ee23f14abd8d868d0a
Ji6ran <ji6rans@gmail.com>
2021-10-30 06:06:27 -0700

    Added About section, navigation tab, and modified path in footer

---
+++ src/components/pages/About.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../../App.css';
+import './About.css'
+
+export default function About() {
+
+  return (
+      <div className='h1'>
+        <h1>About</h1>
+        <div className='body'>
+          <p> <span> Joe Wankleman</span> </p>
+          <p> <span> Jibran Ahmed</span> </p>
+          <p><span> Adrian Unruh </span> </p>
+          <p><span> Elise Wong </span> </p>
+          <p><span> William Simcox </span> </p>
+          <p><span> Hailie Fain </span> </p>
+          <p><span> Dylan Usoro </span> </p>
+        </div>
+
+      </div>
+
+  )
+
+}
