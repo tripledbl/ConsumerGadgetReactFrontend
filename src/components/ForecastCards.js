c4257f5d2c52cbc584d2e7bf9b7461934ecf2b16
Adrian Unruh <unruha@uw.edu>
2021-12-01 23:02:53 -0800

    functional forecast card

---
+++ src/components/ForecastCards.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ForecastCard from './ForecastCard'
+import './ForecastCards.css'
+
+function Cards() {
+    return (
+      <div className='cards'>
+        <h1 className='title'>Forecasts</h1>
+        <div className='cards__container'>
+            <ForecastCard
+                name='Order Volume'
+                accuracy='82%'
+                error='4.5'>
+            </ForecastCard>
+            <ForecastCard
+                name='Revenue'
+                accuracy='79%'
+                error='5.8'>
+            </ForecastCard>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Cards;
--- src/components/pages/Models.js
+++ src/components/pages/Models.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import '../../App.css';
+import ForecastCard from '../ForecastCard';
+import ForecastCards from '../ForecastCards'
 
 export default function Services() {
-  return <h1 className='services'>Models</h1>;
+  return (
+    <div>
+      <ForecastCards>
+      </ForecastCards>
+    </div>
+  );
 }
