04851856da57e3bdfcbc164c8f044dbd88dd313c
Adrian Unruh <unruha@uw.edu>
2021-12-01 23:47:14 -0800

    updated cards

--- src/components/pages/Models.js
+++ src/components/pages/Models.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import '../../App.css';
-import ForecastCard from '../ForecastCard';
 import ForecastCards from '../ForecastCards'
 
 export default function Services() {
   return (
-    <div>
+    <div className='background'>
       <ForecastCards>
       </ForecastCards>
     </div>
