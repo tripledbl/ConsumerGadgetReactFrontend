b17c846c688bcfdf11a70b6f17d2dcb2afaf10ec
unruha <56137150+unruha@users.noreply.github.com>
2021-12-03 23:29:18 -0800

    fixed production redirect

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -6,7 +6,7 @@ const STYLES = ['btn--primary', 'btn--outline'];
 
 const SIZES = ['btn--medium', 'btn--large'];
 
-const MODEL_PAGE_URL = 'http://localhost:3000/models'
+const MODEL_PAGE_URL = 'https://predictant.herokuapp.com/models'
 
 export const LoginButton = ({
   children,
