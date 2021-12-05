e3ff3d0b261bab70ac43dd71daf9f3b425cc8559
unruha <56137150+unruha@users.noreply.github.com>
2021-12-04 16:25:01 -0800

    react hook dummy api call

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -2,7 +2,22 @@ import React from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 
+const baseURL = process.env.REACT_APP_BASE_URL
+
 export default class Calendar extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { pastOrders: [] }
+    }
+
+    componentDidMount() {
+        this.Calendar()
+    }
+
+    Calendar() {
+        console.log('MAKING API CALL: ' + baseURL);
+    }
 
     render(){
         return(
