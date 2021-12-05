96bb8f3bffd812dea99aab3e9e3fa702ba332327
unruha <56137150+unruha@users.noreply.github.com>
2021-12-05 02:30:24 -0800

    predictions working

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 
 const baseURL = process.env.REACT_APP_BASE_URL
 const orders_token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlFWcEdtZXZ0VDVvSE1uVC01d0oyMSJ9.eyJpc3MiOiJodHRwczovL3ByZWRpY3RhbnQudXMuYXV0aDAuY29tLyIsInN1YiI6IjlWdlh3ZnBOZDlTRWkyZ1l0OEVCRXhqY1k3blJWYmxaQGNsaWVudHMiLCJhdWQiOiJodHRwczovL3ByZWRpY3RhbnQtYmFja2VuZC5oZXJva3VhcHAuY29tL29yZGVycyIsImlhdCI6MTYzODY2NTMxMiwiZXhwIjoxNjM4NzUxNzEyLCJhenAiOiI5VnZYd2ZwTmQ5U0VpMmdZdDhFQkV4amNZN25SVmJsWiIsImd0eSI6ImNsaWVudC1jcmVkZW50aWFscyJ9.kjETxJwxeP81jKIfjyXkMoVFEaMzf8tjSX48Z4hBh18kELRnhKabYCzdANGGeKzSDbrTzNjw9umfSLgIrs4Ss6ZVNzUZsCnNaMVcVDIxX-cHqvVgu_ODktB3Fa01lm5GY7V99x0G8H6pFk_QqhsBcFq1zns34Rf2cYFyizC2JPFe_ZGcBJJJyhgzKUrwlvlH_VjIBgM48M5hSQqPXcs11q-Xds20WR2wFDErzRjJP4E7pD-DAlLeJQYuWFnNH4wYHArW5QX1Z1VnO4XnHCqw_sWCdOA-Y7PeKmWtcEx20wMG6Eqm24LfIwuY1FBPB428pWynxGDjWG9EMjsDSd_hhg'
-const users_token = ''
+const users_token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlFWcEdtZXZ0VDVvSE1uVC01d0oyMSJ9.eyJpc3MiOiJodHRwczovL3ByZWRpY3RhbnQudXMuYXV0aDAuY29tLyIsInN1YiI6IkpTTXROa2UyM1ZaMHYwTDR1RlFuTnVXODdlOFNLSUVnQGNsaWVudHMiLCJhdWQiOiJodHRwczovL3ByZWRpY3RhbnQtYmFja2VuZC5oZXJva3VhcHAuY29tL3VzZXIiLCJpYXQiOjE2Mzg2OTQyNjUsImV4cCI6MTYzODc4MDY2NSwiYXpwIjoiSlNNdE5rZTIzVlowdjBMNHVGUW5OdVc4N2U4U0tJRWciLCJndHkiOiJjbGllbnQtY3JlZGVudGlhbHMifQ.R9OeO9Dv6zSq2UDX1zSS6gVstEDlJ0o6_R_bRV22C0Eo0kc7WUFguDSOr03v9IcK3sWS-Ud3hHt9vjSwZKys60xJeAWB-Sq4jFuLukd6kvEHCG4CZMZ3UZMbF8bS-vqgePCXiZSn4c9rM9EZwblpGSTzH403Q6Tc50zS8p2vy-dOlkWhg9bXWabDdmtUp_gfSnc0oXJiFwg5AFDtk9q19_nKqffDx98yG4aM2Z43tIKUZNREMTBkQ326TiYrWNTQukNWpNXZ1-JGvfEBaNi_20pav0LL-gT-tkQS7-qTGTdbcaWE4ZFiE19PQ7KsjunwJPgXPYZJ7XLEDJSOzkHvhA'
 const crabtreeId = process.env.REACT_APP_CRABTREE_USER_ID
 const TWO_WEEKS = 14
 
@@ -13,7 +13,10 @@ export default class Calendar extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = { pastOrders: [] }
+        this.state = { 
+            pastOrders: [],
+            predictionOrders: []
+        }
     }
 
     componentDidMount() {
@@ -29,8 +32,7 @@ export default class Calendar extends React.Component {
 
         // get prediction order data
         let today = new Date();
-        today = this.formatDate(today);
-        // this.getPredictions(today);
+        this.getPredictions(today);
     }
 
     formatDate(date) {
@@ -48,25 +50,26 @@ export default class Calendar extends React.Component {
     }
 
     // gives 14 days worth of customer volume predictions starting with today
-    getPredictions(today) {
-        let counts = [];
+    // day: a date object
+    getPredictions(day) {
         for (let i = 0; i < TWO_WEEKS; i++) {
+            if (day.getDay() === 1 || day.getDate() === 2) {
+                continue;
+            }
+            let strDay = this.formatDate(day)
             axios({
                 method: 'GET',
-                url: baseURL + '/user/' + crabtreeId + "/prediction",
+                url: baseURL + '/user/' + crabtreeId + "/prediction/" + strDay,
                 headers: {'Authorization': 'Bearer ' + users_token },
-                date: today
-            }).then(dates => {
-                let counts = [];
-                for (const date in dates.data) {
-                    let obj = {
-                        'title': 'Order Count: ' + dates.data[date],
-                        'date': date
-                    }
-                    counts.push(obj)
+            }).then(res => {
+                let obj = {
+                    title: 'prediction: ' + Math.round(res.data['prediction']),
+                    date: strDay
                 }
-                this.setState({ pastOrders: counts });
-            });
+                let joined = this.state.predictionOrders.concat(obj);
+                this.setState({ predictionOrders: joined });
+            })
+            day.setDate(day.getDate() + 1)
         }
     }
 
@@ -95,7 +98,7 @@ export default class Calendar extends React.Component {
             <FullCalendar  
                 defaultView="dayGridMonth" 
                 plugins={[ dayGridPlugin ]}
-                events={this.state.pastOrders}
+                events={this.state.pastOrders.concat(this.state.predictionOrders)}
             />
         )
     }
